d441c320aeae674e6e8f28fa1a1cf65b11896c1e
Xerus <27jf@web.de>
2019-01-31 01:53:49 +0100

    Update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,13 @@
-import com.github.jengelman.gradle.plugins.shadow.internal.JavaJarExec
-import org.gradle.internal.os.OperatingSystem
 import com.github.breadmoirai.GithubReleaseTask
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.internal.os.OperatingSystem
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.dsl.Coroutines
-import java.io.ByteArrayOutputStream
-import java.nio.file.*
-import java.util.Scanner
+import java.util.*
 
 buildscript {
 	dependencies {
-		classpath("com.squareup.okhttp3:okhttp:3.12.0")
-		classpath("com.j256.simplemagic:simplemagic:1.10")
+		classpath("com.squareup.okhttp3:okhttp:3.12.1")
+		classpath("com.j256.simplemagic:simplemagic:1.14")
 		classpath("org.zeroturnaround:zt-exec:1.10")
 	}
 }
@@ -19,24 +15,26 @@ buildscript {
 val isUnstable = properties["release"] == null
 val commitNumber = Scanner(Runtime.getRuntime().exec("git rev-list --count HEAD").inputStream).next()
 version = "dev" + commitNumber +
-		"-" + Scanner(Runtime.getRuntime().exec("git rev-parse --short HEAD").inputStream).next()
+	"-" + Scanner(Runtime.getRuntime().exec("git rev-parse --short HEAD").inputStream).next()
 file("src/resources/version").writeText(version as String)
 
 plugins {
-	kotlin("jvm") version "1.3.11"
+	kotlin("jvm") version "1.3.20"
 	application
-	id("com.github.johnrengelman.shadow") version "4.0.3"
+	id("com.github.johnrengelman.shadow") version "4.0.4"
 	id("com.github.ben-manes.versions") version "0.20.0"
 	id("com.github.breadmoirai.github-release") version "2.2.3"
 }
 
 // source directories
 sourceSets {
-	getByName("main") {
+	main {
 		java.srcDir("src/main")
 		resources.srcDir("src/resources")
 	}
-	getByName("test").java.srcDir("src/test")
+	test {
+		java.srcDir("src/test")
+	}
 }
 
 application {
@@ -53,7 +51,7 @@ repositories {
 dependencies {
 	implementation(kotlin("reflect"))
 	
-	implementation("com.github.Xerus2000.util", "javafx", "766520d3a38c9188e342dba0adf643028566db01")
+	implementation("com.github.Xerus2000.util", "javafx", "d21bec5cc61b2def24072084255f9340d3d25c77")
 	implementation("org.controlsfx", "controlsfx", "8.40.+")
 	
 	implementation("ch.qos.logback", "logback-classic", "1.2.+")
@@ -61,7 +59,7 @@ dependencies {
 	
 	implementation("com.github.Bluexin", "drpc4k", "16b0c60")
 	implementation("org.apache.httpcomponents", "httpmime", "4.5.+")
-	implementation("com.google.apis", "google-api-services-sheets", "v4-rev20181116-1.27.0")
+	implementation("com.google.apis", "google-api-services-sheets", "v4-rev20190109-1.28.0")
 	
 	val junitVersion = "5.3.2"
 	testCompile("org.junit.jupiter", "junit-jupiter-api", junitVersion)
@@ -81,11 +79,11 @@ tasks {
 	
 	val shadowJar by getting(ShadowJar::class) {
 		group = MAIN
-		classifier = ""
-		destinationDir = file(".")
+		archiveClassifier.set("")
+		destinationDirectory.set(file("."))
 		doFirst {
-			destinationDir.listFiles().forEach {
-				if (it.name.endsWith("jar"))
+			destinationDirectory.get().asFile.listFiles().forEach {
+				if(it.name.endsWith("jar"))
 					it.delete()
 			}
 		}
@@ -111,7 +109,7 @@ tasks {
 		dependsOn(shadowJar, githubRelease)
 		group = MAIN
 		
-		val path = file("../monsterutilities-extras/website/downloads/" + if (isUnstable) "unstable" else "latest")
+		val path = file("../monsterutilities-extras/website/downloads/" + if(isUnstable) "unstable" else "latest")
 		val pathLatest = path.resolveSibling("latest") // TODO temporary workaround until real release
 		doFirst {
 			path.writeText(version.toString())
@@ -119,11 +117,11 @@ tasks {
 			println("Uploading release to server...")
 		}
 		
-		val s = if (OperatingSystem.current().isWindows) "\\" else ""
+		val s = if(OperatingSystem.current().isWindows) "\\" else ""
 		commandLine("lftp", "-c", """set ftp:ssl-allow true; set ssl:verify-certificate no;
 			open -u ${project.properties["credentials.ftp"]} -e $s"
 			cd /www/downloads/files; put $jarFile;
-			cd /www/downloads; ${if (project.properties["noversion"] == null) "put $path; put $pathLatest;" else ""}
+			cd /www/downloads; ${if(project.properties["noversion"] == null) "put $path; put $pathLatest;" else ""}
 			quit$s" monsterutilities.bplaced.net""".filter { it != '\t' && it != '\n' })
 	}
 	
@@ -131,7 +129,7 @@ tasks {
 		kotlinOptions.jvmTarget = "1.8"
 	}
 	
-	withType<Test> {
+	test {
 		useJUnitPlatform()
 	}
 	
