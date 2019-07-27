4116b5c2e8d1feb14b5e1182710f9cfffcb8bdaf
Xerus <27jf@web.de>
2019-07-27 15:50:32 +0200

    test: expand APIUtils.find test

--- src/test/xerus/monstercat/api/APIUtilsTest.kt
+++ src/test/xerus/monstercat/api/APIUtilsTest.kt
@@ -1,16 +1,30 @@
 package xerus.monstercat.api
 
-import io.kotlintest.matchers.collections.shouldNotContain
+import io.kotlintest.inspectors.forAll
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
-import xerus.monstercat.api.response.ArtistRel
+import kotlinx.coroutines.runBlocking
+import xerus.ktutil.printIt
+
+data class SongSearch(val searchTerm: String, val expectedTerm: String? = null)
 
 internal class APIUtilsTest: StringSpec({
 	
-	"find Edge of the World by Karma Fields" {
-		val edge = APIUtils.find("Edge Of The World", "Karma Fields")!!
-		edge.artists shouldNotContain ArtistRel("Razihel")
-		edge.artistsTitle shouldBe "Karma Fields"
+	"APIUtils.find" {
+		arrayOf(
+			SongSearch("Karma Fields - Edge of the World"),
+			SongSearch("Julian Calor - Monster (feat. Trove)", "Julian Calor feat. Trove - Monster"),
+			SongSearch("Rootkit - Voyage (Kage Remix)")
+		).forAll {
+			runBlocking {
+				val search = it.searchTerm.split(" - ")
+				val result = APIUtils.find(search[1], search[0])
+				val expected = (it.expectedTerm ?: it.searchTerm).split(" - ", " (", ")")
+				result shouldBe Cache.getTracks().find {
+					it.artistsTitle == expected[0] && it.title == expected[1] && (expected.size < 3 || it.version == expected[2])
+				}.printIt()
+			}
+		}
 	}
 	
 })
