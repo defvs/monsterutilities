5382a924d4972475d5f8e9add73450f808258390
Daniel Thirion <defvs.daniel@gmail.com>
2019-08-04 14:07:45 +0200

    style(playlist): reformat code

--- src/main/xerus/monstercat/api/Playlist.kt
+++ src/main/xerus/monstercat/api/Playlist.kt
@@ -8,9 +8,12 @@ import javafx.scene.input.MouseButton
 import javafx.scene.layout.VBox
 import javafx.scene.media.MediaPlayer
 import javafx.stage.Modality
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import mu.KotlinLogging
-import xerus.ktutil.ifNotNull
 import xerus.ktutil.ifNull
 import xerus.ktutil.javafx.*
 import xerus.ktutil.javafx.properties.SimpleObservable
@@ -31,7 +34,7 @@ object Playlist {
 			tracks.indexOf(Player.activeTrack.value).takeUnless { i -> i == -1 }
 		}, Player.activeTrack, tracks)
 	}
-
+	
 	val repeat = SimpleBooleanProperty(false)
 	val shuffle = SimpleBooleanProperty(false)
 	
@@ -68,7 +71,7 @@ object Playlist {
 		tracks.removeAt(index ?: tracks.size - 1)
 	}
 	
-	fun clear(){
+	fun clear() {
 		history.clear()
 		tracks.clear()
 	}
@@ -82,6 +85,7 @@ object Playlist {
 		val index = (Math.random() * tracks.size).toInt()
 		return if(index == currentIndex.value && tracks.size > 1) getNextTrackRandom() else tracks[index]
 	}
+	
 	fun getNextTrack(): Track? {
 		val cur = currentIndex.value
 		return when {
@@ -94,7 +98,7 @@ object Playlist {
 	
 	fun addAll(tracks: ArrayList<Track>, asNext: Boolean = false) {
 		tracks.removeAll(tracks)
-		if (asNext)
+		if(asNext)
 			tracks.addAll(currentIndex.value?.let { it + 1 } ?: 0, tracks)
 		else
 			tracks.addAll(tracks)
@@ -102,37 +106,38 @@ object Playlist {
 }
 
 object PlaylistManager {
-	suspend fun loadPlaylist(apiConnection: APIConnection){
+	suspend fun loadPlaylist(apiConnection: APIConnection) {
 		val tracks = apiConnection.getTracks()
 		tracks?.forEachIndexed { index, track ->
 			val found = Cache.getTracks().find { it.id == track.id }
-			if (found != null)
+			if(found != null)
 				tracks[index] = found
 			else {
 				tracks.removeAt(index)
 				logger.error("Skipped track ${track.artistsTitle} - ${track.title} with id ${track.id}: Not found in the cache.")
 			}
 		}
-		if (tracks != null && tracks.isNotEmpty()) {
-			if (Player.player?.status != MediaPlayer.Status.DISPOSED)
+		if(tracks != null && tracks.isNotEmpty()) {
+			if(Player.player?.status != MediaPlayer.Status.DISPOSED)
 				Player.reset()
 			Playlist.setTracks(tracks)
 		}
 	}
-
+	
 	/** Opens the playlist manager dialog
 	 * Allows to load, save, and manage playlists stored on Monstercat.com
 	 */
-	fun playlistDialog(){
+	fun playlistDialog() {
 		val connection = APIConnection("api", "playlist").fields(ConnectPlaylist::class)
-
+		
 		val parent = VBox()
 		val stage = App.stage.createStage("Monstercat.com Playlists", parent)
 		stage.initModality(Modality.WINDOW_MODAL)
 		val connectTable = TableView<ConnectPlaylist>()
-
+		
 		// Common playlist functions
 		fun load() = GlobalScope.launch { loadPlaylist(APIConnection("api", "playlist", connectTable.selectionModel.selectedItem.id, "tracks")) }
+		
 		fun loadUrl(): Job {
 			val subParent = VBox()
 			val subStage = stage.createStage("Load from URL", subParent)
@@ -145,7 +150,7 @@ object PlaylistManager {
 				try {
 					loadPlaylist(APIConnection("api", "playlist", playlistId!!, "tracks"))
 					onFx { subStage.close() }
-				}catch (e: Exception){ // FIXME : This breaks everything; if we get in the catch, the error message will show, but job.invokeOnComplete will still work and everything will be closed.
+				} catch(e: Exception) { // FIXME : This breaks everything; if we get in the catch, the error message will show, but job.invokeOnComplete will still work and everything will be closed.
 					onFx {
 						monsterUtilities.showAlert(Alert.AlertType.WARNING, "No playlist found", content = "No playlists were found at ${textField.text}.")
 					}
@@ -153,24 +158,25 @@ object PlaylistManager {
 				}
 			}
 			
-			subParent.addRow(createButton("Load"){
+			subParent.addRow(createButton("Load") {
 				playlistId = textField.text.substringAfterLast("/")
-				if (playlistId!!.length == 24){
+				if(playlistId!!.length == 24) {
 					job.start()
 					(it.source as Button).let { button ->
 						button.isDisable = true
 						button.text = "Loading..."
 					}
-				}else{
+				} else {
 					monsterUtilities.showAlert(Alert.AlertType.WARNING, "Playlist URL invalid", content = "${textField.text} is not a valid URL.")
 				}
-			}, createButton("Cancel"){
+			}, createButton("Cancel") {
 				subStage.close()
 				job.cancel()
 			})
 			subStage.show()
 			return job
 		}
+		
 		fun replace() = GlobalScope.launch { APIConnection.editPlaylist(connectTable.selectionModel.selectedItem.id, tracks = Playlist.tracks) }
 		fun delete() = GlobalScope.launch { APIConnection.editPlaylist(connectTable.selectionModel.selectedItem.id, deleted = true) }
 		fun new(): Job {
@@ -182,23 +188,24 @@ object PlaylistManager {
 			subParent.children.addAll(textField, publicTick)
 			
 			val job = GlobalScope.launch(start = CoroutineStart.LAZY) {
-				APIConnection.createPlaylist(textField.text.let { if (it.isBlank()) "New Playlist" else it }, Playlist.tracks, publicTick.isSelected)
+				APIConnection.createPlaylist(textField.text.let { if(it.isBlank()) "New Playlist" else it }, Playlist.tracks, publicTick.isSelected)
 				onFx { subStage.close() }
 			}
 			
-			subParent.addRow(createButton("Create"){
+			subParent.addRow(createButton("Create") {
 				job.start()
 				(it.source as Button).let { button ->
 					button.isDisable = true
 					button.text = "Loading..."
 				}
-			}, createButton("Cancel"){
+			}, createButton("Cancel") {
 				subStage.close()
 				job.cancel()
 			})
 			subStage.show()
 			return job
 		}
+		
 		fun rename(): Job {
 			val subParent = VBox()
 			val subStage = stage.createStage("Rename Playlist", subParent)
@@ -207,34 +214,34 @@ object PlaylistManager {
 			subParent.add(textField)
 			
 			val job = GlobalScope.launch(start = CoroutineStart.LAZY) {
-				APIConnection.editPlaylist(connectTable.selectionModel.selectedItem.id, name = textField.text.let { if (it.isBlank()) "Unnamed" else it })
+				APIConnection.editPlaylist(connectTable.selectionModel.selectedItem.id, name = textField.text.let { if(it.isBlank()) "Unnamed" else it })
 				onFx { subStage.close() }
 			}
 			
-			subParent.addRow(createButton("Rename"){
+			subParent.addRow(createButton("Rename") {
 				job.start()
 				(it.source as Button).let { button ->
 					button.isDisable = true
 					button.text = "Loading..."
 				}
-			}, createButton("Cancel"){
+			}, createButton("Cancel") {
 				subStage.close()
 				job.cancel()
 			})
 			subStage.show()
 			return job
 		}
-
+		
 		val playlists = FXCollections.observableArrayList<ConnectPlaylist>()
 		fun updatePlaylists() {
 			playlists.clear()
-			if (APIConnection.connectValidity.value != ConnectValidity.NOGOLD && APIConnection.connectValidity.value != ConnectValidity.GOLD){
+			if(APIConnection.connectValidity.value != ConnectValidity.NOGOLD && APIConnection.connectValidity.value != ConnectValidity.GOLD) {
 				connectTable.placeholder = Label("Please connect using connect.sid in the downloader tab.")
-			}else{
+			} else {
 				connectTable.placeholder = Label("Loading...")
 				GlobalScope.launch {
 					val results = connection.getPlaylists()
-					if (results != null && results.isNotEmpty())
+					if(results != null && results.isNotEmpty())
 						playlists.addAll(results)
 					else
 						onFx {
@@ -243,19 +250,19 @@ object PlaylistManager {
 				}
 			}
 		}
-
+		
 		connectTable.apply {
 			columnResizePolicy = TableView.CONSTRAINED_RESIZE_POLICY
 			columns.addAll(TableColumn<ConnectPlaylist, String>("Name") { it.value.name },
-					TableColumn<ConnectPlaylist, String>("Size") { it.value.tracks.size.toString() })
+				TableColumn<ConnectPlaylist, String>("Size") { it.value.tracks.size.toString() })
 			items = playlists
 			updatePlaylists()
-
+			
 			selectionModel.selectionMode = SelectionMode.SINGLE
-			setOnMouseClicked { if (it.button == MouseButton.PRIMARY && it.clickCount == 2) load() }
+			setOnMouseClicked { if(it.button == MouseButton.PRIMARY && it.clickCount == 2) load() }
 			
 			val publicMenuItem = CheckMenuItem("Public", {
-				if (connectTable.selectionModel.selectedItem != null) {
+				if(connectTable.selectionModel.selectedItem != null) {
 					GlobalScope.launch {
 						APIConnection.editPlaylist(connectTable.selectionModel.selectedItem.id, public = it)
 						onFx { updatePlaylists() }
@@ -273,10 +280,10 @@ object PlaylistManager {
 				publicMenuItem.isSelected = selectionModel.selectedItem?.public ?: false
 			}
 		}
-
+		
 		parent.add(Label("Tip : You can right-click a playlist to edit it without the window closing each time !"))
 		parent.addRow(
-			createButton("Load"){
+			createButton("Load") {
 				connectTable.selectionModel.selectedItem ?: return@createButton
 				load().invokeOnCompletion { onFx { stage.close() } }
 				(it.source as Button).let { button ->
@@ -284,8 +291,8 @@ object PlaylistManager {
 					button.text = "Loading..."
 				}
 			},
-			createButton("From URL..."){ loadUrl().invokeOnCompletion { it.ifNull { onFx { stage.close() } } } },
-			createButton("Save into selected"){
+			createButton("From URL...") { loadUrl().invokeOnCompletion { it.ifNull { onFx { stage.close() } } } },
+			createButton("Save into selected") {
 				connectTable.selectionModel.selectedItem ?: return@createButton
 				replace().invokeOnCompletion { onFx { stage.close() } }
 				(it.source as Button).let { button ->
@@ -293,8 +300,8 @@ object PlaylistManager {
 					button.text = "Loading..."
 				}
 			},
-			createButton("Save as new..."){ new().invokeOnCompletion { it.ifNull { onFx { stage.close() } } } },
-			createButton("Cancel"){ stage.close() })
+			createButton("Save as new...") { new().invokeOnCompletion { it.ifNull { onFx { stage.close() } } } },
+			createButton("Cancel") { stage.close() })
 		parent.fill(connectTable, 0)
 		stage.show()
 	}
