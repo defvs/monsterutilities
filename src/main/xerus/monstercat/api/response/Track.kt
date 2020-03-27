07168d645ddd065415b4ec97c042cddf466ce3b3
Daniel Thirion <defvs.daniel@gmail.com>
2020-03-27 12:12:54 +0100

    fix(api): convert Track data class to API v2

--- src/main/xerus/monstercat/api/response/Track.kt
+++ src/main/xerus/monstercat/api/response/Track.kt
@@ -1,27 +1,20 @@
 package xerus.monstercat.api.response
 
 import com.google.api.client.util.Key
+import xerus.ktutil.splitTitleTrimmed
 import xerus.monstercat.api.splitArtists
 import xerus.monstercat.api.splitTitle
-import xerus.monstercat.api.splitTitleTrimmed
 import java.util.Collections.emptyList
 
 open class Track: MusicItem() {
 	
-	@Key("_id")
-	override var id: String = ""
-	@Key
-	var artistsTitle: String = ""
-	@Key
-	override var title: String = ""
-	@Key
-	var version: String = ""
-	@Key
-	var albums: List<Album> = emptyList()
-	@Key("artistRelationships")
-	var artists: List<ArtistRel> = emptyList()
-	@Key
-	var bpm: Double? = null
+	@Key override var id: String = ""
+	@Key var artistsTitle: String = ""
+	@Key override var title: String = ""
+	@Key var version: String = ""
+	@Key var artists: List<ArtistRel> = emptyList()
+	@Key var bpm: Double? = null
+	@Key var streamable: Boolean = false
 	
 	var artistsSplit: List<String> = emptyList()
 	var titleClean: String = ""
@@ -35,10 +28,7 @@ open class Track: MusicItem() {
 	var albumArtists = ""
 	var albumId = ""
 	var albumName = ""
-	var trackNumber = -1
-	
-	val streamHash: String?
-		get() = albums.find { it.streamHash.isNotEmpty() }?.streamHash
+	@Key var trackNumber = -1
 	
 	val isAlbumMix
 		get() = title.contains("Album Mix")
@@ -51,7 +41,6 @@ open class Track: MusicItem() {
 			albumArtists = release.renderedArtists
 			albumName = release.title
 			albumId = release.catalogId
-			trackNumber = albums.find { it.albumId == release.id }?.trackNumber ?: -1
 		}
 		
 		artistsTitle = formatArtists(artistsTitle)
@@ -89,5 +78,5 @@ open class Track: MusicItem() {
 	override fun hashCode() = id.hashCode()
 	
 	fun debugString(): String =
-		"Track(id='$id', artistsTitle='$artistsTitle', title='$title', albums=$albums, artists=$artists, bpm=$bpm, artistsSplit=$artistsSplit, titleClean='$titleClean', remix='$remix', feat='$feat', extra='$extra', splitTitle=$splitTitle, release=${if(::release.isInitialized) release else null}, albumArtists='$albumArtists', albumId='$albumId', albumName='$albumName', trackNumber=$trackNumber)"
+		"Track(id='$id', artistsTitle='$artistsTitle', title='$title', artists=$artists, bpm=$bpm, artistsSplit=$artistsSplit, titleClean='$titleClean', remix='$remix', feat='$feat', extra='$extra', splitTitle=$splitTitle, release=${if(::release.isInitialized) release else null}, albumArtists='$albumArtists', albumId='$albumId', albumName='$albumName', trackNumber=$trackNumber)"
 }
