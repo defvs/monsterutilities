14d34f5ed31afc8f75c85274ebcc398b8246d276
Daniel THIRION <defvs.daniel@gmail.com>
2020-11-19 19:17:23 +0100

    feat(covers): revert covers cache file extension hardcode

--- src/main/xerus/monstercat/api/Covers.kt
+++ src/main/xerus/monstercat/api/Covers.kt
@@ -16,7 +16,7 @@ object Covers {
 	private fun coverCacheFile(coverUrl: String, size: Int): File {
 		coverCacheDir.mkdirs()
 		val newFile = coverCacheDir.resolve(coverUrl.substringBeforeLast('/').substringAfterLast('/').replaceIllegalFileChars())
-		return coverCacheDir.resolve("${newFile.nameWithoutExtension}x$size.jpg") // FIXME : Do not hardcode extension
+		return coverCacheDir.resolve("${newFile.nameWithoutExtension}x$size")
 	}
 		
 	/** Returns an Image of the cover in the requested size using caching.
