8d5881986da050d20a73dd614f751470b5a3821d
Daniel THIRION <defvs.daniel@gmail.com>
2020-12-09 10:51:01 +0100

    fix: change cookie name from connect.sid to cid

--- src/main/xerus/monstercat/api/response/Session.kt
+++ src/main/xerus/monstercat/api/response/Session.kt
@@ -5,8 +5,8 @@ import com.google.api.client.util.Key
 /** Session obtained by /self/session */
 data class Session(@Key var user: User? = null, @Key var settings: Settings? = null)
 
-/** User infos, included in a session if the connect.sid is valid */
+/** User infos, included in a session if the cid is valid */
 data class User(@Key var hasGold: Boolean = false)
 
-/** User settings, included in a session if the connect.sid is valid */
+/** User settings, included in a session if the cid is valid */
 data class Settings(@Key var preferredDownloadFormat: String = "")
